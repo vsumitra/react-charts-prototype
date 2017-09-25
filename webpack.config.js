f910fb5d3ef1f2aef8ebfdeb46fb397056ca56c6
vsumitra <vsumitra@costar.com>
2017-09-25 16:18:09 -0700

    initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const combineLoaders = require('webpack-combine-loaders');
+
+const config = {
+  resolve: {
+    modules: [
+      path.resolve('./lib'),
+      path.resolve('./node_modules')
+    ]
+  },
+  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+      {
+        test: /\.css$/,
+        loader: combineLoaders([
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader',
+            query: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]'
+            }
+          }
+        ])
+      }
+    ]
+  }
+};
+
+module.exports = config;
