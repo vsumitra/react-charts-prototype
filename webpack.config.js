fa0c33bfe7953c9b7cd49fd6c2e57296b0747216
vsumitra <vsumitra@gmail.com>
2017-10-08 18:59:07 -0700

    Added zoom buttons and date range selector.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const combineLoaders = require('webpack-combine-loaders');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   resolve: {
@@ -18,22 +16,10 @@ const config = {
     rules: [{ 
       test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' 
     }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract(
-        'style-loader', 
-        combineLoaders([{
-          loader: 'css-loader',
-          query: {
-            modules: true,
-            localIdentName: '[name]__[local]___[hash:base64:5]'
-          }
-        }])
-      )
+      test: /\.css/,
+      loaders: ['style-loader', 'css-loader?modules&localIdentName=[local]---[hash:base64:5]', 'cssnext-loader']
     }]
-  },
-  plugins: [
-    new ExtractTextPlugin('styles.css')
-  ]
+  }
 };
 
 module.exports = config;
